a0e73e1e3fca4ec6e7a92f790ac80bcd4ebf6b66
tfkhdyt <tfkhdyt@proton.me>
2023-01-07 22:17:22 +0700

    fix(card): adjust padding

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -6,9 +6,9 @@
 </script>
 
 <div
-	class="bg-slate-800/50 m-6 p-8 md:p-12 rounded-xl backdrop-blur-md shadow-lg"
+	class="bg-slate-800/50 m-6 p-8 md:px-10 md:py-8 rounded-xl backdrop-blur shadow-lg"
 >
-	<h1 class="text-gray-300 text-3xl font-bold mb-4 text-center">M/M/s Queue Theory Calculator</h1>
+	<h1 class="text-gray-300 text-xl md:text-3xl font-bold mb-4 text-center">M/M/s Queue Theory Calculator</h1>
 	<Form />
 
 	{#if $po && $l && $w && $lq && $wq && $pq && $rho}
