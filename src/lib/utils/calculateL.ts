9dbacc36d626c5f9bc4aa1a7f1d937581cc6be66
tfkhdyt <tfkhdyt@proton.me>
2023-01-07 13:53:59 +0700

    feat(utils): add calculateL function

---
+++ src/lib/utils/calculateL.ts
@@ -0,0 +1,13 @@
+import { calculatePo } from './calculatePo';
+import { factorial } from './factorial';
+
+export function calculateL(arrivalRate: number, serviceRate: number, numberOfServers: number) {
+	const a = arrivalRate * serviceRate * (arrivalRate / serviceRate) ** numberOfServers;
+	const b = factorial(numberOfServers - 1) * (numberOfServers * serviceRate - arrivalRate) ** 2;
+	const po = calculatePo(arrivalRate, serviceRate, numberOfServers);
+	const c = arrivalRate / serviceRate;
+
+	const result = (a / b) * po + c;
+  
+  return result;
+}
