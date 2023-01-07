16e53694e525777ff6290584fe90420c73a44462
tfkhdyt <tfkhdyt@proton.me>
2023-01-07 14:25:12 +0700

    test: add constants for testing

---
+++ src/lib/utils/_test/constants.ts
@@ -0,0 +1,18 @@
+type TestPayload = {
+  arrivalRate: number,
+  serviceRate: number,
+  numberOfServers: number,
+}
+
+export const test1: TestPayload = {
+  arrivalRate: 10,
+  serviceRate: 5,
+  numberOfServers: 4
+}
+
+export const test2: TestPayload = {
+  arrivalRate: 5,
+  serviceRate: 10,
+  numberOfServers: 5
+}
+
