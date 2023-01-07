824fa35a18dd7068cc891031d5a1049fdbef30b2
tfkhdyt <tfkhdyt@proton.me>
2023-01-07 13:54:40 +0700

    style(calculatePo): add empty line to separate arrange and assert

--- src/lib/utils/_test/calculatePo.test.ts
+++ src/lib/utils/_test/calculatePo.test.ts
@@ -5,6 +5,7 @@ describe('calculatePo function', () => {
 	it('calculate Po correctly', () => {
 		const po1 = calculatePo(10, 5, 4);
 		const po2 = calculatePo(5, 15, 5);
+
 		expect(po1).toBeCloseTo(0.1304);
 		expect(po2).toBeCloseTo(0.7165);
 	});
