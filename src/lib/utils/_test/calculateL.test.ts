b0270457685aeaabe06d71a54b1cbc103d7d777e
tfkhdyt <tfkhdyt@proton.me>
2023-01-07 14:29:51 +0700

    test(calculateL): refactor test requirement to using constants instead

--- src/lib/utils/_test/calculateL.test.ts
+++ src/lib/utils/_test/calculateL.test.ts
@@ -1,10 +1,11 @@
 import { describe, expect, it } from 'vitest';
 import { calculateL } from '../calculateL';
+import { test1, test2 } from './constants';
 
 describe('calculateL function', () => {
 	it('should calculateL correctly', () => {
-		const l1 = calculateL(10, 5, 4);
-		const l2 = calculateL(5, 15, 5);
+		const l1 = calculateL(test1.arrivalRate, test1.serviceRate, test1.numberOfServers);
+		const l2 = calculateL(test2.arrivalRate, test2.serviceRate, test2.numberOfServers);
 
 		expect(l1).toBeCloseTo(2.1739);
 		expect(l2).toBeCloseTo(0.333);
