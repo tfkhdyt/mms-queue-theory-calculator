0ec4de308856538f2b4ea40ecd65b0cb91f960b7
tfkhdyt <tfkhdyt@proton.me>
2023-01-07 22:16:49 +0700

    fix(buttons): adjust hover effect

--- src/lib/components/buttons/SubmitButton.svelte
+++ src/lib/components/buttons/SubmitButton.svelte
@@ -1,6 +1,6 @@
 <button
 	type="submit"
-	class="mt-2 w-[75%] text-center rounded-lg font-bold inline-block bg-[#6600ff] px-8 py-3 text-white transition hover:scale-105 hover:shadow-xl focus:outline-none focus:ring focus:ring-[#5509c9] active:bg-[#5e05e4]"
+	class="mt-2 w-[75%] text-center rounded-lg font-bold inline-block bg-[#6600ff] px-8 py-3 text-white transition hover:scale-105 hover:shadow-xl focus:outline-none focus:ring focus:ring-[#5509c9] hover:outline-none hover:ring hover:ring-[#5509c9] active:bg-[#5e05e4]"
 >
 	Calculate
 </button>
