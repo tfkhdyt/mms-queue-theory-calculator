2a22c412f4c6132ef55825991ad82ff50a34c4c2
tfkhdyt <tfkhdyt@proton.me>
2023-01-08 19:18:36 +0700

    refactor(result): create results state and iter over it

--- src/lib/components/Result.svelte
+++ src/lib/components/Result.svelte
@@ -1,30 +1,54 @@
 <script lang="ts">
 	import { po, l, w, lq, wq, pq, rho } from '$stores/output';
+
+	$: results = [
+		{
+			title: 'The probability that there are no customers in the system',
+			content: `Po = ${$po} = `,
+			alternateResult: ($po * 100).toFixed(2) + '%'
+		},
+		{
+			title: 'The average number of customers in the queuing system',
+			content: `L = ${$l} = `,
+			alternateResult: $l.toFixed(2) + ' customers'
+		},
+		{
+			title: 'Time customers spend in queues',
+			content: `W = ${$w} = `,
+			alternateResult: ($w * 60).toFixed(2) + ' minutes'
+		},
+		{
+			title: 'The number of customers in the queue line',
+			content: `Lq = ${$lq} = `,
+			alternateResult: $lq.toFixed(2) + ' customers'
+		},
+		{
+			title: 'Time spent waiting to be served',
+			content: `Wq = ${$wq} = `,
+			alternateResult: ($wq * 60).toFixed(2) + ' minutes'
+		},
+		{
+			title: 'The probability that the server is busy and the customer is waiting',
+			content: `Pq = ${$pq} = `,
+			alternateResult: ($pq * 100).toFixed(2) + '%'
+		},
+		{
+			title: 'Server utilization',
+			content: `p = ${$rho} = `,
+			alternateResult: ($rho * 100).toFixed(2) + '%'
+		}
+	];
 </script>
 
 <div>
 	<h1 class="mb-4 text-center text-xl font-bold text-gray-300 md:text-3xl">Result</h1>
 	<div class="text text-gray-300">
 		<ul class="text-md ml-6 list-disc space-y-2 font-mono lg:ml-0">
-			<li title="The probability that there are no customers in the system">
-				Po = {$po} = <b>{($po * 100).toFixed(2) + '%'}</b>
-			</li>
-			<li title="The average number of customers in the queuing system">
-				L = {$l} = <b>{$l.toFixed(2) + ' customers'}</b>
-			</li>
-			<li title="Time customers spend in queues">
-				W = {$w} = <b>{($w * 60).toFixed(2) + ' minutes'}</b>
-			</li>
-			<li title="The number of customers in the queue line">
-				Lq = {$lq} = <b>{$lq.toFixed(2) + ' customers'}</b>
-			</li>
-			<li title="Time spent waiting to be served">
-				Wq = {$wq} = <b>{($wq * 60).toFixed(2) + ' minutes'}</b>
-			</li>
-			<li title="The probability that the server is busy and the customer is waiting">
-				Pq = {$pq} = <b>{($pq * 100).toFixed(2) + '%'}</b>
-			</li>
-			<li title="Server utilization">p = {$rho} = <b>{($rho * 100).toFixed(2) + '%'}</b></li>
+			{#each results as { title, content, alternateResult } (title)}
+				<li {title}>
+					{content}<b class="text-lg underline underline-offset-4">{alternateResult}</b>
+				</li>
+			{/each}
 		</ul>
 	</div>
 </div>
