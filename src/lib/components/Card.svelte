2243b15d00dcc349ab383fdf36f8958db82c3226
tfkhdyt <tfkhdyt@proton.me>
2023-01-08 09:11:15 +0700

    fix(background): fix background not really fixed

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -5,7 +5,7 @@
 	import Result from './Result.svelte';
 </script>
 
-<main>
+<main class="absolute">
 	<div class="bg-slate-800/50 m-6 p-8 md:px-10 md:py-8 rounded-xl backdrop-blur shadow-lg">
 		<h1 class="text-gray-300 text-xl md:text-3xl font-bold mb-4 text-center">
 			M/M/s Queue Theory Calculator
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,26 @@
 <script>
-	import Card from "$lib/components/Card.svelte";
-
+	import Card from '$lib/components/Card.svelte';
 </script>
+
 <svelte:head>
 	<title>M/M/s Queue Theory Calculator</title>
 </svelte:head>
-<main class="grid place-items-center min-h-screen">
-  <Card />
+
+<div class="bg" />
+<main class="grid place-items-center min-h-screen min-w-screen content">
+	<Card />
 </main>
+
+<style>
+	.content {
+		background-image: url('../assets/img/background.svg');
+		background-repeat: no-repeat;
+		background-position: center;
+		height: 100vh;
+		width: 100vw;
+		position: fixed;
+		z-index: -1;
+		background-size: cover;
+		/* background-attachment: fixed; */
+	}
+</style>
