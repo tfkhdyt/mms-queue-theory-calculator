8d1fa1a03d613fea546bd3e0d974b9287b31c17f
tfkhdyt <tfkhdyt@proton.me>
2023-01-08 11:32:38 +0700

    fix(buttons): remove some focus event

--- src/lib/components/buttons/ResetButton.svelte
+++ src/lib/components/buttons/ResetButton.svelte
@@ -1,6 +1,6 @@
 <button
 	type="reset"
-	class="mt-2 w-auto text-center rounded-lg font-bold inline-block bg-red-600 px-8 py-3 text-white transition hover:scale-105 hover:shadow-xl focus:outline-none focus:ring focus:ring-red-700 hover:outline-none hover:ring hover:ring-red-700 active:bg-red-700"
+	class="mt-2 w-auto text-center rounded-lg font-bold inline-block bg-rose-600 px-8 py-3 text-white transition hover:scale-105 hover:shadow-xl hover:outline-none hover:ring hover:ring-rose-700 active:bg-rose-700 focus:outline-none"
 >
 	Reset
 </button>
