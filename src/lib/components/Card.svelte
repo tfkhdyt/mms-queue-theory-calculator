e4d817ecd3bb86814b041df8d603db837688fa0b
tfkhdyt <tfkhdyt@proton.me>
2023-01-08 11:33:12 +0700

    feat(card): set card to 2 column in lg breakpoint

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -1,18 +1,26 @@
 <script lang="ts">
-	import { po, l, w, lq, wq, pq, rho } from '$stores/output';
+	import { po } from '$stores/output';
 
 	import Form from './Form.svelte';
 	import Result from './Result.svelte';
+
+	$: isResult = Boolean($po);
 </script>
 
 <main>
-	<div class="bg-slate-800/50 m-6 p-8 md:px-10 md:py-8 rounded-xl backdrop-blur shadow-lg">
-		<h1 class="text-gray-300 text-xl md:text-3xl font-bold mb-4 text-center">
-			M/M/s Queue Theory Calculator
-		</h1>
-		<Form />
+	<div
+		class={`bg-slate-800/50 m-6 p-8 md:px-10 md:py-8 rounded-xl backdrop-blur shadow-lg grid grid-cols-1 gap-16 ${
+			isResult && 'lg:grid-cols-2'
+		}`}
+	>
+		<div>
+			<h1 class="text-gray-300 text-xl md:text-3xl font-bold mb-4 text-center">
+				M/M/s Queue Theory Calculator
+			</h1>
+			<Form />
+		</div>
 
-		{#if $po && $l && $w && $lq && $wq && $pq && $rho}
+		{#if isResult}
 			<Result />
 		{/if}
 	</div>
@@ -20,9 +28,9 @@
 		Created by <a
 			class="link link-underline link-underline-black font-medium"
 			href="https://tfkhdyt.my.id"
-      rel="noreferrer"
+			rel="noreferrer"
 			target="_blank"
-      title="NIM: 301200032">tfkhdyt</a
+			title="NIM: 301200032">tfkhdyt</a
 		>
 	</p>
 </main>
