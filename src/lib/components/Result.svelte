12779679a08a08489f2f2e875f7efbc96b538a30
tfkhdyt <tfkhdyt@proton.me>
2023-01-08 09:32:06 +0700

    feat(result): add title hint

--- src/lib/components/Result.svelte
+++ src/lib/components/Result.svelte
@@ -8,13 +8,13 @@
 	<h1 class="text-gray-300 text-xl md:text-3xl font-bold mb-4 text-center">Result</h1>
 	<div class="text-gray-300 text">
 		<ul class="space-y-2 text-xl font-mono">
-			<li>Po : {$po}</li>
-			<li>L : {$l}</li>
-			<li>W : {$w}</li>
-			<li>Lq : {$lq}</li>
-			<li>Wq : {$wq}</li>
-			<li>Pq : {$pq}</li>
-			<li>p : {$rho}</li>
+			<li title="The probability that there are no customers in the system">Po : {$po}</li>
+			<li title="The average number of customers in the queuing system">L : {$l}</li>
+			<li title="Time customers spend in queues">W : {$w}</li>
+			<li title="The number of customers in the queue line">Lq : {$lq}</li>
+			<li title="Time spent waiting to be served">Wq : {$wq}</li>
+			<li title="The probability that the server is busy and the customer is waiting">Pq : {$pq}</li>
+			<li title="Server utilization">p : {$rho}</li>
 		</ul>
 	</div>
 </div>
