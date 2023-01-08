e7e614cefb0a20cb280fd75e9388e1fb6690afbf
tfkhdyt <tfkhdyt@proton.me>
2023-01-08 11:35:53 +0700

    feat(result): show alternative format

--- src/lib/components/Result.svelte
+++ src/lib/components/Result.svelte
@@ -1,20 +1,28 @@
 <script lang="ts">
 	import { po, l, w, lq, wq, pq, rho } from '$stores/output';
-	import { fade } from 'svelte/transition';
 </script>
 
-<div transition:fade>
-	<hr class="my-10 border-gray-600" />
+<div>
 	<h1 class="text-gray-300 text-xl md:text-3xl font-bold mb-4 text-center">Result</h1>
 	<div class="text-gray-300 text">
-		<ul class="space-y-2 text-xl font-mono">
-			<li title="The probability that there are no customers in the system">Po : {$po}</li>
-			<li title="The average number of customers in the queuing system">L : {$l}</li>
-			<li title="Time customers spend in queues">W : {$w}</li>
-			<li title="The number of customers in the queue line">Lq : {$lq}</li>
-			<li title="Time spent waiting to be served">Wq : {$wq}</li>
-			<li title="The probability that the server is busy and the customer is waiting">Pq : {$pq}</li>
-			<li title="Server utilization">p : {$rho}</li>
+		<ul class="space-y-2 text-md font-mono list-disc ml-6 lg:ml-0">
+			<li title="The probability that there are no customers in the system">
+				Po = {$po} = <b>{($po * 100).toFixed(2) + '%'}</b>
+			</li>
+			<li title="The average number of customers in the queuing system">
+				L = {$l} = <b>{$l.toFixed(2) + ' customers'}</b>
+			</li>
+			<li title="Time customers spend in queues">W = {$w} = <b>{($w * 60).toFixed(2) + ' minutes'}</b></li>
+			<li title="The number of customers in the queue line">
+				Lq = {$lq} = <b>{$lq.toFixed(2) + ' customers'}</b>
+			</li>
+			<li title="Time spent waiting to be served">
+				Wq = {$wq} = <b>{($wq * 60).toFixed(2) + ' minutes'}</b>
+			</li>
+			<li title="The probability that the server is busy and the customer is waiting">
+				Pq = {$pq} = <b>{($pq * 100).toFixed(2) + '%'}</b>
+			</li>
+			<li title="Server utilization">p = {$rho} = <b>{($rho * 100).toFixed(2) + '%'}</b></li>
 		</ul>
 	</div>
 </div>
